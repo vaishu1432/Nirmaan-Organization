530135f21e241d813804d187bc733c569edf9fa7
vaishu1432 <132999642+vaishu1432@users.noreply.github.com>
2024-11-21 12:57:06 +0530

    Add files via upload
    day 2

---
+++ Task2.java
@@ -0,0 +1,24 @@
+public class Task2{
+
+public static void main(String args[]){
+    
+String name1="vaishu";
+String name2="vaishna";
+String adjective1="joyful";
+String adjective2="happy";
+String noun1="tree";
+String noun2="world";
+String verb1="dance";
+String noun3="music";
+String noun4="people";
+String adjective3="enjoyment";
+String place1="Uk";
+String noun5="coffee";
+String noun6="nature";
+int number=2040;
+
+String letter = "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a " + adjective2+ " day!' Outside, a bunch of " + noun1+ "s were protesting to keep " + noun2 + " in stores. They began to" + verb1 + " to the rhythm of the " + noun3 + ", which made all the " + noun4 + "s very " + adjective3 + ".Concerned, " + name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped " +name1 + " in  puddle of frozen "+noun5 + ". " + name1 + " woke up in the year " + number + ", in a world here " + noun6 + "s ruled the world.";
+
+   System.out.print(letter);
+}
+}
