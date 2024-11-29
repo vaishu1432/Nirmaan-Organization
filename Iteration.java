2b0167a60e3d6f7362b5cb0ce419382b9f9dc697
vaishu1432 <132999642+vaishu1432@users.noreply.github.com>
2024-11-29 13:13:32 +0530

    Add files via upload

---
+++ Iteration.java
@@ -0,0 +1,19 @@
+package Day6;
+
+public class Iteration {
+	public static void main(String []args) {
+		
+//		for(int i=1;i<=200;i++) {
+//			if (i==101) {
+//				break;
+//			}else {
+//				System.out.println(i);
+//			}
+//		}
+		
+
+
+String name ="apple";
+System.out.println(name.toUpperCase());
+}
+}
