8e684197190154a595a69a83b8d4a37f58ba0986
vaishu1432 <132999642+vaishu1432@users.noreply.github.com>
2024-12-03 12:55:42 +0530

    day8

---
+++ Nestedloop.java
@@ -0,0 +1,14 @@
+package Day7;
+
+public class Nestedloop {
+	public static void main (String[]args) {
+		for(int i=0;i<=4;i++) {
+			
+		for (int j=0;j<=3;j++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+
+}
+}
