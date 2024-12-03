c93be687da006402593868229e84377ff8843043
vaishu1432 <132999642+vaishu1432@users.noreply.github.com>
2024-12-03 12:56:29 +0530

    day8 task2

---
+++ Length.java
@@ -0,0 +1,15 @@
+package Day7;
+
+public class Length {
+
+	public static void main(String[]args) {
+		String Actor="Surya";
+		String str=new String("Megna");
+		if(Actor.equals(str)) {
+			System.out.println("Megna death");
+		}
+		else {
+			System.out.println("he is feeling");
+	}
+}
+}
