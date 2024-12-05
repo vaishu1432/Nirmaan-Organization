b423b18f9eb5bcc612a96198010ba1f4a0d24714
vaishu1432 <132999642+vaishu1432@users.noreply.github.com>
2024-12-05 11:26:50 +0530

    day 9 calculator

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+package Day9;
+
+public class Calculator {
+	
+	void Addition(int a,int b) {
+		System.out.println("Addition" +(a+b));
+	}
+	void Subtraction(int a,int b) {
+		System.out.println("Subtraction"+(a-b));
+	}
+	void Multiplication(int a,int b) {
+		System.out.println("Multiplication"+(a*b));
+	}
+	void Divition(int a,int b) {
+		System.out.println("Divition"+(a/b));
+	}
+	void Moduls(int a,int b) {
+		System.out.println("Moduls"+(a%b));
+	}
+}
