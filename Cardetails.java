3dfa438fbffc4cc3ce464983569d718038e1fa2b
vaishu1432 <132999642+vaishu1432@users.noreply.github.com>
2024-12-05 11:28:34 +0530

    day10 task2

---
+++ Cardetails.java
@@ -0,0 +1,14 @@
+package Day10;
+
+public class Cardetails {
+public static void main(String args[]) {
+	Car V =new Car ("Vaisu");
+	V.displayCardetails();
+	Car S =new Car("Santhini,2000");
+	
+    S.displayCardetails();
+	
+	
+
+}
+}
